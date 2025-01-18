54d01f91f36a8f25c360bf148e35370a87e5b01f
PARASMANIKHUNTE <137080546+PARASMANI-KHUNTE@users.noreply.github.com>
2025-01-18 20:08:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 // Set up Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", 
+    origin: "https://linkify-4max.onrender.com", 
     methods: ["GET", "POST"],
   },
 });
